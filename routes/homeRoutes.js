ea1a62e8d2ff2467a269b7f616bd8289c7817bf0
mbright90 <michael.bright90@icloud.com>
2024-10-18 17:55:10 +0100

    Create home routes and log in functionality

---
+++ routes/homeRoutes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const passport = require("passport");
+const userController = require("../controllers/userController");
+
+const router = new express.Router();
+
+// --- POST ROUTES --- //
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+  }),
+);
+
+router.post("/register", async (req, res, next) => {
+  try {
+    await userController.addUserToDatabase(req.body);
+    res.redirect("/log-in");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// --- GET ROUTES --- //
+
+router.get("/log-in", (req, res) => {
+  res.render("forms/log-in-form");
+});
+
+router.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+router.get("/register", (req, res) => {
+  res.render("forms/register-form");
+});
+
+router.get("/", (req, res) => {
+  const user = req.user;
+  if (user) {
+    res.render("home", { name: user.first_name });
+  } else {
+    res.render("landing-page");
+  }
+});
+
+module.exports = router;
