972eb6ddda5e48ed8f56a125d30ca07d2cb20209
mbright90 <michael.bright90@icloud.com>
2024-10-20 10:40:06 +0100

    implement post routes and cors

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const cors = require("cors");
 const db = require("./config/database");
-const router = require("./routes/homeRoutes");
-
 const PgStore = require("connect-pg-simple")(session);
 
-const app = express();
+// Routers
+const homeRouter = require("./routes/homeRoutes");
+const postRouter = require("./routes/postRoutes");
 
+const app = express();
 app.set("view engine", "ejs");
 
+app.use(cors({ origin: "http://localhost:8080", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -39,7 +42,8 @@ require("./config/passport");
 app.use(passport.session());
 
 // --- ROUTES --- //
-app.use("/", router);
+app.use("/posts/", postRouter);
+app.use("/", homeRouter);
 
 // --- ERROR HANDLING --- //
 
