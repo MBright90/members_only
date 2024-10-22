07765f0429b11b2f05c44689880d76895999e965
mbright90 <michael.bright90@icloud.com>
2024-10-22 10:44:00 +0100

    Implement session store with prisma

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const cors = require("cors");
-const db = require("./config/database");
-const PrismaStore = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 
 // Routers
 const homeRouter = require("./routes/homeRoutes");
@@ -14,12 +14,15 @@ const postRouter = require("./routes/postRoutes");
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + "/public"));
 app.use(cors({ origin: "http://localhost:8080", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // --- SESSION MIDDLEWARE --- //
-const sessionStore = new PrismaStore(db, {
+const db = new PrismaClient();
+
+const sessionStore = new PrismaSessionStore(db, {
   checkPeriod: 2 * 60 * 1000, //ms
   dbRecordIdIsSessionId: true,
   dbRecordIdFunction: undefined,
@@ -49,17 +52,8 @@ app.use("/", homeRouter);
 
 // --- SERVER --- //
 const PORT = process.env.PORT;
-app
-  .listen(3000, () => {
-    console.log("-------------------------");
-    console.log(`Listening on PORT:${PORT}...`);
-    console.log("-------------------------");
-  })
-  .then(async () => {
-    db.$disconnect();
-  })
-  .catch(async (err) => {
-    console.log(err);
-    db.$disconnect();
-    process.exit(1);
-  });
+app.listen(3000, () => {
+  console.log("-------------------------");
+  console.log(`Listening on PORT:${PORT}...`);
+  console.log("-------------------------");
+});
