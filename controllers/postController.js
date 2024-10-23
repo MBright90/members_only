68e692d62a86822fee8382a41f33415bd8a9e3ff
mbright90 <michael.bright90@icloud.com>
2024-10-23 16:52:41 +0100

    Convert to using post prisma model

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,17 +1,14 @@
-const posts = require("../models/post");
 const prisma = require("../config/database");
 
 module.exports.addPostToDatabase = async function (req, res) {
   const { title, content } = req.body;
   const authorId = req.user.id;
-  const author = req.user.username;
 
   try {
     await prisma.post.create({
       data: {
         title,
         content,
-        author,
         authorId,
       },
     });
@@ -26,7 +23,9 @@ module.exports.getUsersPosts = async function (req, res) {
   const authorId = req.params.userId;
 
   try {
-    const result = await posts.getPostsByUserId(authorId);
+    const result = await prisma.post.findMany({
+      where: { authorId },
+    });
     if (result.length > 0) {
       res.render("user-posts", { username: result[0].author, posts: result });
     } else {
@@ -40,7 +39,25 @@ module.exports.getUsersPosts = async function (req, res) {
 
 module.exports.getRecentPosts = async function (req, res) {
   try {
-    const result = await posts.getRecentPosts();
+    console.log("Finding posts..");
+    const result = await prisma.post.findMany({
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        createdAt: true,
+        author: {
+          select: {
+            username: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 20,
+    });
+    console.log(result);
     res.render("recent-posts", { posts: result });
   } catch (err) {
     res.status(500).json({ err: err });
