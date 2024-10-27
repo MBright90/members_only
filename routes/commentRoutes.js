6141faaf7a4c86b15a50737f6dfa82ed0e847e11
mbright90 <michael.bright90@icloud.com>
2024-10-27 12:53:52 +0000

    Add routes for updating reports by admin

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 const commentController = require("../controllers/commentController");
-const { isPaid } = require("../lib/authMiddleware");
+const { isPaid, isAuth, isAdmin } = require("../lib/authMiddleware");
 
 const router = new express.Router();
 router.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // --- POST ROUTES --- //
 router.post("/add-comment", isPaid, commentController.addCommentToDatabase);
+
+router.post("/report-:commentId", isAuth, commentController.postReportComment);
+
+router.post(
+  "/comments/resolve-report-delete-:reportId",
+  isAdmin,
+  commentController.deleteCommentFromReport,
+);
+
+router.post(
+  "/comments/resolve-report-remove-:reportId",
+  isAdmin,
+  commentController.resolveCommentFromReport,
+);
+
+// --- COMMENT ROUTES --- //
+router.get(
+  "/report-comment-form-:commentId",
+  isAuth,
+  commentController.getReportForm,
+);
 
 module.exports = router;
