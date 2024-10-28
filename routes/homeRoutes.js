707449d8d2e588720b97249b880912f8b0764347
mbright90 <michael.bright90@icloud.com>
2024-10-28 12:44:15 +0000

    Create logic for retrieving popular post

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const passport = require("passport");
 const cors = require("cors");
+const prisma = require("../config/database");
+
 const postController = require("../controllers/postController");
 const commentController = require("../controllers/commentController");
 const userController = require("../controllers/userController");
 const { isAdmin } = require("../lib/authMiddleware");
+const { formatTimeAgo } = require("../lib/timeUtils");
 
 const router = new express.Router();
 router.use(cors({ origin: "http://localhost:3000", credentials: true }));
@@ -61,13 +64,67 @@ router.get("/dashboard", (req, res) => {
   res.redirect("dashboard/posts");
 });
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const user = req.user;
+
   if (user) {
-    res.render("home", {
-      user,
-      posts: [],
-    });
+    try {
+      // Retrieve recent post
+      const result = await prisma.post.findMany({
+        where: {
+          authorId: user.id,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 1,
+      });
+      const recentPost = result[0];
+      if (recentPost) {
+        recentPost.createdAgo = formatTimeAgo(recentPost.createdAt);
+      }
+
+      const now = Date.now();
+
+      // Get post which has most new comments created in past 24 hours
+      const activeCommentsId = await prisma.comment.groupBy({
+        by: ["postId"],
+        where: {
+          createdAt: { gte: new Date(now - 24 * 60 * 60 * 1000) },
+        },
+        _count: { postId: true },
+        orderBy: { _count: { postId: "desc" } },
+        take: 1,
+      });
+
+      const popularPost = await prisma.post.findFirst({
+        where: {
+          id: activeCommentsId[0].postId,
+        },
+        include: {
+          author: {
+            select: {
+              id: true,
+              username: true,
+            },
+          },
+        },
+      });
+
+      popularPost.createdAgo = formatTimeAgo(popularPost.createdAt);
+
+      res.render("home", {
+        user,
+        recentPost: recentPost || null,
+        popularPost,
+      });
+    } catch (err) {
+      console.log(`Error retrieving home information: ${err}`);
+      res.render("/errors/error", {
+        user,
+        errMsg: [""],
+      });
+    }
   } else {
     res.render("landing-page");
   }
